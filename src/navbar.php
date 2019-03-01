8b71fe45e97dfa878068d77b4fb24f48bc8f0f0e
JustinRisney <31742964+JustinRisney@users.noreply.github.com>
2019-03-01 11:30:20 -0800

    Operation Jacob's Playhouse has commenced

---
+++ src/navbar.php
@@ -0,0 +1,14 @@
+<style type="text/css">
+	nav {
+		background-color: rgba(204, 232, 254);
+		opacity: 1;
+		height: 70px;
+	}
+</style>
+
+<nav class="navbar" style="background-color: rgba(204, 232, 254, 1);">
+	<ul class="navbar-nav">
+		<li class="nav-item">Yuh</li>
+	</ul>
+</nav>
+
