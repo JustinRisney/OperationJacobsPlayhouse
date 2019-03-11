a13c6a62397bd627f13fd7a6d3ce2a10faf82559
JustinRisney <31742964+JustinRisney@users.noreply.github.com>
2019-03-11 11:33:33 -0700

    Finna style this bitch pt 1

--- src/index.php
+++ src/index.php
@@ -3,72 +3,21 @@
 <head>
 	
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
+  <link rel="stylesheet" href="/stylesheet/index.css">
 	<title>Home</title>
-		
+	<style>
+  
+</style>
 </head>
 <body>
+    <?php include 'navbar.php';?>
 		<h1>Jacob Boyce's Playhouse</h1>
 
 	<p>Please help this depressed boi out and be his Wamen.  Someone stole his heart, <b>GET IT BACK!</b></p>
-
-	<?php include './navbar.php';?>
-	<div></div>
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-	<style>
-* {
-  box-sizing: border-box;
-}
-
-.column {
-  float: left;
-  width: 33.33%;
-  padding: 5px;
-}
-
-
-.row::after {
-  content: "";
-  clear: both;
-  display: table;
-}
-	</style>
-<div class="row">
-  <div class="column">
-    <img src="jacob.png" alt="Jacob Boyce's Face" style="width:100%">
-  </div>
-  <div class="column">
-    <img src="jacob.png" alt="Jacob Boyce's Face" style="width:100%">
-  </div>
-  <div class="column">
-    <img src="jacob.png" alt="Jacob Boyce's Face" style="width:100%">
-  </div>
-</div>
-<div class="row">
-  <div class="column">
-    <img src="jacob.png" alt="Jacob Boyce's Face" style="width:100%">
-  </div>
-  <div class="column">
-    <img src="jacob.png" alt="Jacob Boyce's Face" style="width:100%">
-  </div>
-  <div class="column">
-    <img src="jacob.png" alt="Jacob Boyce's Face" style="width:100%">
-  </div>
-</div>
-<div class="row">
-  <div class="column">
-    <img src="jacob.png" alt="Jacob Boyce's Face" style="width:100%">
-  </div>
-  <div class="column">
-    <img src="jacob.png" alt="Jacob Boyce's Face" style="width:100%">
-  </div>
-  <div class="column">
-    <img src="jacob.png" alt="Jacob Boyce's Face" style="width:100%">
-  </div>
-</div>
-
 	<p>Jacob Boyce is a handsome, nerdy, romantic, and determined when he chooses.  He is looking for a nice wamen.  He takes care of his parents and sister daily and falls on his bed in fatigue every 11:53pm.  He constantly is searching for sleep and needs it.  He needs a wamen to help break this cycle.</p>
+  <div></div>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
--- src/navbar.php
+++ src/navbar.php
@@ -1,14 +1,35 @@
 <style type="text/css">
 	nav {
-		background-color: rgba(204, 232, 254);
-		opacity: 1;
-		height: 70px;
+		background-color: rgba(204, 232, 254, 1);
+		
+		/**height: 70px;**/
+		transition: 90s;
 	}
+	nav:hover {
+		/**background-color: rgba(204, 232, 254, 1);**/
+		background-color: #FF69B4;
+		
+	}
+
 </style>
 
-<nav class="navbar" style="background-color: rgba(204, 232, 254, 1);">
+<nav class="navbar navbar-expand-sm fixed-top">
 	<ul class="navbar-nav">
-		<li class="nav-item">Yuh</li>
+		<li class="nav-brand">
+			<a class="nav-link" href="">
+				<img src="">
+			</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="/photos/">
+				<p>Photos</p>
+			</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="/contact/">
+				<p>Contact Me</p>
+			</a>
+		</li>
 	</ul>
 </nav>
 
